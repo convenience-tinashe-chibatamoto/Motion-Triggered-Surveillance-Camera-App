9765740242fed527897a0011fef2156454b84779
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-27 20:07:46 +0200

    Implement threading
    -Implemented threading to improve the application's performance
    -Since motion detection and image capture can occur simultaneously, I implemented threading so that one thread can continuously monitor for motion while another captures images or video frames, ensuring that no motion is missed while processing previous frames.
    -The main function runs in a separate thread, continuously monitoring for motion.
    -When motion is detected, it captures an image and starts another thread to handle the email sending process.
    -This ensures that the main thread is free to continue motion detection without being blocked by the email sending process.

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import time
 import glob
 import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
+
 first_frame = None
 status_list = []
 count = 1
 
+
 def clean_folder():
+    print("clean_folder function started")
     images = glob.glob("images/*.png")
     for image in images:
         os.remove(image)
-    
+    print("clean_folder function ended")
 
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -26,13 +31,13 @@ while True:
         first_frame = gray_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    
     cv2.imshow("My video", dil_frame)
-    
+
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+
     for contour in contours:
         if cv2.contourArea(contour) < 5000:
             continue
@@ -46,17 +51,29 @@ while True:
             index = int(len(all_images) / 2)
             image_with_object = all_images[index]
 
+
+
     status_list.append(status)
     status_list = status_list[-2:]
 
-    
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        email_thread.daemon = True
 
-    
+        email_thread.start()
+
+
+
     print(status_list)
 
-    
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
+
+clean_thread.start()
